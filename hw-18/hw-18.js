2b127a752193608347f4a03d6b23797ebd751d56
first-thirteenth <tpatata13@gmail.com>
2026-02-10 12:34:44 +0100

    new file: .vscode/settings.json

---
+++ hw-18/hw-18.js
@@ -0,0 +1,18 @@
+//Базовый уровень
+//Задача 1
+function getSum (n) {
+    let sum = 0
+    for (i = 1; i <= n; i++) {
+        sum = sum + n
+    }
+    return(sum) 
+}
+
+console.log(getSum(3))
+
+
+
+
+
+
+
