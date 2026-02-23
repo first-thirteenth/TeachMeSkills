8c85e6d26e6e02fc709ea13d00f4bd9923ca225b
first-thirteenth <tpatata13@gmail.com>
2026-02-23 14:53:36 +0100

    modified js_for_children.js

--- Practice/js_for_children/js_for_children.js
+++ Practice/js_for_children/js_for_children.js
@@ -1,2 +1,30 @@
-//1
+// //1
+// /*Превратить строку "эЙ, кАК деЛа?" в "Эй, как дела?".*/
+// Как я решил.
+// let bedString = 'эЙ, кАК деЛа?'
+// let lowercase = bedString.toLowerCase()
+// let bigLetter = lowercase.slice(0, 1).toUpperCase()
+// let newString = bigLetter + lowercase.slice(1)
+// console.log(newString)
+// //Так решил бы профи.
+// let bedString = 'эЙ, кАК деЛа?'
+// let newString = bedString.charAt(0).toUpperCase() + bedString.slice(1).toLowerCase()
+// console.log(newString)
+
+//2
+let accompanied = false
+function automaticCinema(age) {
+    if (age >= 12 || (age < 12 && accompanied)) {
+        return 'Проходите!'
+    }
+    return 'Прохода НЕТ!'
+}
+
+console.log(automaticCinema(13))
+
+
+
+
+
+
 
