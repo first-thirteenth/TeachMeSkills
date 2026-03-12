5f214aa074a4193f97d291f0c236185eb801b2ac
first-thirteenth <tpatata13@gmail.com>
2026-03-12 22:31:07 +0100

    Tack 1, 2

---
+++ hw-21/hw.21.js
@@ -0,0 +1,58 @@
+// // Tack 1
+// const fibonacci = [
+//   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
+// ];
+
+// //first way
+// function arrayElementsDeclaration(value) {
+//    console.log('value ', value);
+// }
+
+// fibonacci.forEach(arrayElementsDeclaration);
+
+// //second way
+// const arrayElementsArrow = fibonacci.forEach((value) => {
+//     console.log('value: ', value);
+// })
+
+     
+// //Tack 2
+// const users = ["Darya", "Masha", "Denis", "Vitaliy", "Polina", "Anton"];
+ 
+// //first way
+// function methodMap(user, index) {
+//     return `member, ${index + 1}: ${user}`;
+// }
+
+// const member1 = users.map(methodMap)
+// console.log(member1);
+
+// //second way
+// const member2 = users.map((user, index) => `member ${index + 1}: ${user}`);
+
+// console.log(member2);
+
+
+//Tack 3
+const numbers = [7, -4, 32, -90, 54, 32, -21];
+
+
+
+
+
+
+
+
+
+
+// function foo(bar) {
+//     let name = 'Andrei';
+//     bar(name)
+// }
+
+// function bar(name) {
+//     console.log('Hello ' + name);
+// }
+   
+// foo(bar);
+
