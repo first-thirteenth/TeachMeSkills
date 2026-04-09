faf913c1383cff35dc1b00413dbe0b41208b8ef4
first-thirteenth <tpatata13@gmail.com>
2026-04-09 13:33:49 +0100

    styles card

--- hw-23/todo_list/src/scripts/main.js
+++ hw-23/todo_list/src/scripts/main.js
@@ -28,7 +28,7 @@ if (root) {
   const btnDellAll = createElement('button', 'control-panel__btn-dell', 'Delete All' );
   controlPanel.appendChild(btnDellAll);
 
-  const inputTodoText = createElement('input', 'control-panel__input');
+  const inputTodoText = createElement('input', 'control-panel__input', '', 'todo-input');
   inputTodoText.type = 'text';
   inputTodoText.placeholder = 'Enter todo ...';
   controlPanel.appendChild(inputTodoText);
@@ -41,22 +41,25 @@ if (root) {
   const card = createElement('div', 'card');
   container.appendChild(card);
 
-  const cardCheckbox = createElement('checkbox', 'card__checkbox');
+  const cardCheckbox = createElement('input', 'card__checkbox');
   cardCheckbox.type = 'checkbox'
   card.appendChild(cardCheckbox);
   
-  const cardInput = createElement('p', 'card__input');
+  const cardInput = createElement('p', 'card__input', 'Todo text');
   card.appendChild(cardInput);
 
-  const cardButtonDell = createElement('button', 'card__btn-dell');
+  const cardButtonDell = createElement('button', 'card__btn-dell', 'Delete');
   card.appendChild(cardButtonDell);
 
+  const now = new Date();
   const cardTime = createElement('time', 'card__time');
+  cardTime.dateTime = now.toISOString(); 
+  cardTime.textContent = now.toLocaleString('ru-RU', {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric'
+  });
   card.appendChild(cardTime);
-
-  
-
-
 }
 
 
